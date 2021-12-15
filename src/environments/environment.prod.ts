eb16736eb4163a721bf013c5933e697a6ae40e0e
DaveLyndrex <davelyndrex@gmail.com>
2021-12-15 17:34:37 +0800

    frontend

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,21 @@
+export const environment = {
+  production: true,
+  firebase: {
+    // apiKey: 'AIzaSyDj-oH_Is4D2NQcoBFSaEZmH9aklVdUGD8',
+    // authDomain: 'cn-locator-d6562.firebaseapp.com',
+    // databaseURL:
+    //   'https://cn-locator-d6562-default-rtdb.asia-southeast1.firebasedatabase.app',
+    // projectId: 'cn-locator-d6562',
+    // storageBucket: 'cn-locator-d6562.appspot.com',
+    // messagingSenderId: '568188875173',
+    // appId: '1:568188875173:web:784c960881baff239d384c',
+    apiKey: 'AIzaSyBxUK-CRXCnTSeT0OpJmKEhxVg0W9uQ8kk',
+    authDomain: 'cn-locator1.firebaseapp.com',
+    projectId: 'cn-locator1',
+    storageBucket: 'cn-locator1.appspot.com',
+    messagingSenderId: '623124256714',
+    appId: '1:623124256714:web:ef1662a2d4f7081ff32133',
+    databaseURL:
+      'https://cn-locator1-default-rtdb.asia-southeast1.firebasedatabase.app',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,35 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  firebase: {
+    // apiKey: 'AIzaSyDj-oH_Is4D2NQcoBFSaEZmH9aklVdUGD8',
+    // authDomain: 'cn-locator-d6562.firebaseapp.com',
+    // databaseURL:
+    //   'https://cn-locator-d6562-default-rtdb.asia-southeast1.firebasedatabase.app',
+    // projectId: 'cn-locator-d6562',
+    // storageBucket: 'cn-locator-d6562.appspot.com',
+    // messagingSenderId: '568188875173',
+    // appId: '1:568188875173:web:784c960881baff239d384c',
+    apiKey: 'AIzaSyBxUK-CRXCnTSeT0OpJmKEhxVg0W9uQ8kk',
+    authDomain: 'cn-locator1.firebaseapp.com',
+    projectId: 'cn-locator1',
+    storageBucket: 'cn-locator1.appspot.com',
+    messagingSenderId: '623124256714',
+    appId: '1:623124256714:web:ef1662a2d4f7081ff32133',
+    databaseURL:
+      'https://cn-locator1-default-rtdb.asia-southeast1.firebasedatabase.app',
+  },
+  googleMapsKey: 'AIzaSyAT7sLvC1X1qClhNgzhLQ7ZQLTotJCyyEc',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
